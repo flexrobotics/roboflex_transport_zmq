2f84c13c0477ef14fa436ddbce93da24c52426a8
Colin Prepscius <colinprepscius@gmail.com>
2023-11-02 22:46:16 -0400

    lowered min cmake to 3.18

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='roboflex.transport.zmq',
-    version='0.1.6',
+    version='0.1.7',
     description='Roboflex Transport ZMQ Library',
     author='Colin Prepscius',
     author_email='colinprepscius@gmail.com',
